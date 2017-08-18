03d29d5a9b9308dc4c2054abafa6374cb61a2f62
expiritus2 <michaildrazdovich@gmail.com>
2017-08-19 00:25:14 +0300

    Delete ApplicationConfig.java

--- src/main/java/com/dreamproject/config/ApplicationConfig.java
+++
@@ -1,7 +0,0 @@
-package com.dreamproject.config;
-
-public class ApplicationConfig {
-
-    public static final String ACCESS_KEY_ID = "AKIAIAHNO6UDH6DSWX2Q";
-    public static final String SECRET_KEY_ID = "0nPGkY+Cr4VzmxmLnKomucmt6fBWpUaa63SDg6aS";
-}
